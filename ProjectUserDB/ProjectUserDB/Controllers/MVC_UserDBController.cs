df40c1d79d274c04b08757e61f488523965a04c0
DESKTOP-LOUR863\User <Guo1997tw@gmail.com>
2022-11-20 21:37:04 +0800

    Create Create, Update (UserTableCreate, UserTableEdit)

--- ProjectUserDB/ProjectUserDB/Controllers/MVC_UserDBController.cs
+++ ProjectUserDB/ProjectUserDB/Controllers/MVC_UserDBController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
 
@@ -39,6 +41,49 @@ namespace ProjectUserDB.Controllers
             
             if (UserTableDetailsOne == null) { return HttpNotFound(); }
             else { return View(UserTableDetailsOne.FirstOrDefault()); }
+        }
+        public ActionResult UserTableCreate()
+        {
+            return View();
+        }
+        [HttpPost, ActionName("UserTableCreate")]
+        [ValidateAntiForgeryToken]
+        public ActionResult UserTableCreateConfirm(UserTable _UserTable)
+        {
+            if((_UserTable != null) && (ModelState.IsValid))
+            {
+                _MUDBC.UserTables.Add(_UserTable);
+                _MUDBC.SaveChanges();
+
+                return RedirectToAction("UserTableList");
+            }
+
+            return Content("欄位有誤！請返回上一頁並重新填寫。");
+        }
+        public ActionResult UserTableEdit(int? _ID)
+        {
+            if (_ID == null) { return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest); }
+
+            UserTable _UT = _MUDBC.UserTables.Find(_ID);
+
+            if (_UT == null) { return HttpNotFound(); }
+            else { return View(_UT); }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UserTableEdit(UserTable _UserTable)
+        {
+            if (_UserTable == null) { return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest); }
+            
+            if(ModelState.IsValid)
+            {
+                _MUDBC.Entry(_UserTable).State = System.Data.Entity.EntityState.Modified;
+                _MUDBC.SaveChanges();
+
+                return RedirectToAction("UserTableList");
+            }
+
+            return Content("欄位有誤！請返回上一頁並重新填寫。");
         }
     }
 }
